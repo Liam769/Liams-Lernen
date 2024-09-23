3802886aeaa90be8321b5b289653e8b9adb4a8f6
Nick <nikybrine.jung@gmail.com>
2024-09-23 18:57:11 +0200

    Projektdateien hinzufügen.

---
+++ Liams Lernen/Liams Lernen.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a = 0;
+    float b = 0;
+    float c = 0;
+    cout << "mach mathe du huan: ";
+    cin >> a;
+    cout << "\n mach zweite: ";
+    cin >> b;
+    cout << "\n mach drite";
+    cin >> c;
+    cout << a + b + c;
+}
